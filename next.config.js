fc632cf9aa157914f06a90bf196ff7c6d39fd168
Mohan Lu <ml7612@nyu.edu>
2025-02-04 20:37:55 -0500

    remove readme and see what wil happen

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   reactStrictMode: true,
   images: {
-    loader: 'custom', // Use a custom loader
     unoptimized: true, // Disable optimization for static builds
   },
   assetPrefix: './', // Required for GitHub Pages
