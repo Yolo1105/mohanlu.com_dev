8201ccc8f4a318010da0cd1eacfecea56131b835
Mohan Lu <ml7612@nyu.edu>
2025-02-04 21:16:12 -0500

    hehe

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   reactStrictMode: true,
   images: {
+    loader: 'akamai', // Or another supported loader like 'imgix' or 'cloudinary'
+    path: '', // Ensure a custom path or empty string for sta
     unoptimized: true, // Disable optimization for static builds
   },
   assetPrefix: './', // Required for GitHub Pages
